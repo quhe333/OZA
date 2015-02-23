0ec3366eeb6f802b766a9c6c27a2155897c0a853
mrrush2 <michaelrush1@gmail.com>
2015-02-22 18:14:08 -0600

    Ladders and test scene
    Started to create ladders.

---
+++ Assets/Scripts/Ladder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ladder : MonoBehaviour {
+
+	public bool onLadder = false;
+	// Use this for initialization
+	void OnTriggerEnter(Collider2D other)
+	{
+		if (other.tag == "Player") 
+			onLadder = true;
+	}
+	void OnTriggerExit(Collider2D other)
+	{
+		if (other.tag == "Player")
+			onLadder = false;
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
